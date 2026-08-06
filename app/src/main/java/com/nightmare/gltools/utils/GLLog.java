86d38fa4b07cf3a46dc7b5737ce75eccf152321c
alexisplacenciainocencio-maker <alexisplacenciainocencio@gmail.com>
2026-08-06 13:02:21 -0400

    Add files via upload

--- app/src/main/java/com/nightmare/gltools/utils/GLLog.java
+++ app/src/main/java/com/nightmare/gltools/utils/GLLog.java
@@ -1,1 +1,37 @@
+package com.nightmare.gltools.utils;
 
+import android.util.Log;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class GLLog {
+
+    private static final String GLOBAL_TAG = "GLTools2026";
+    private static boolean debug = true;
+
+    public static void setDebug(boolean d) { debug = d; }
+
+    public static void i(@NonNull String tag, @NonNull String msg) {
+        Log.i(GLOBAL_TAG, "[" + tag + "] " + msg);
+    }
+
+    public static void d(@NonNull String tag, @NonNull String msg) {
+        if (debug) Log.d(GLOBAL_TAG, "[" + tag + "] " + msg);
+    }
+
+    public static void w(@NonNull String tag, @NonNull String msg) {
+        Log.w(GLOBAL_TAG, "[" + tag + "] " + msg);
+    }
+
+    public static void e(@NonNull String tag, @NonNull String msg, @Nullable Throwable t) {
+        if (t != null) {
+            Log.e(GLOBAL_TAG, "[" + tag + "] " + msg, t);
+        } else {
+            Log.e(GLOBAL_TAG, "[" + tag + "] " + msg);
+        }
+    }
+
+    public static void e(@NonNull String tag, @NonNull String msg) {
+        e(tag, msg, null);
+    }
+}
